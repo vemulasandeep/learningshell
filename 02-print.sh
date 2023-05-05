a6655e67cc3d1b18f9dd9794912a3ab684b27c1f
Sandeep <sandeep.vemula88@gmail.com>
2023-05-05 22:10:18 +0530

    print test

--- 02-print.sh
+++ 02-print.sh
@@ -18,3 +18,24 @@ echo -e  "Hello,\nHow are you,\nGood day"
 
 echo -e "a\tb"
 
+# print some colors
+#syntax : echo -e "\e[COLmMessage\e[0m"
+# -e --> enable escape sequence
+# /e --> enable col
+# [Col --> give color inout
+# \e[0m --> disable the color
+
+# What are the colors we have ?
+
+#COL      Code         Syntax
+#RED       31           \e[31m
+#GREEN     32           \e[32m
+#YELLOW    33           \e[33m
+#BLUE      34           \e[34m
+#MAGENTA   35           \e[35m
+#CYNA      36           \e[36m
+
+echo -e "\e[31m Hello in RED color"
+echo -e "\e[32m Hello in Green color"
+echo Hello in normal color
+
