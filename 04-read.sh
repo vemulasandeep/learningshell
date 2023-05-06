860fb1fd663b27fc219af4f52380ffe54ffdc1be
Sandeep <sandeep.vemula88@gmail.com>
2023-05-06 12:18:20 +0530

    Variable Command substitution

---
+++ 04-read.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+read -p 'Enter your name : ' name
+echo -e "Hey $name welcome to Shell Scripting training"
+
+read -p 'Enter your phone number details :' phone
+
+echo -e "Thanks for giving your details, will text you with more details on $phone"
+
