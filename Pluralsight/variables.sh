60667e3b635eafd546505d5608145d21509df6b6
Sandeep <sandeep.vemula88@gmail.com>
2023-05-14 22:30:14 +0530

    testing variables

--- Pluralsight/variables.sh
+++ Pluralsight/variables.sh
@@ -2,4 +2,4 @@
 directory=reports1 #creating the variable called directory and giving the value as "reports1"
 mkdir $directory #creating the directory "reports1"
 grep B5 /root/shipments.csv > $directory/B5g
-cat B5g
+cat /reports1/B5g
