16fb75b824cd4dc2c37ee9f27700923382027476
Sandeep <sandeep.vemula88@gmail.com>
2023-05-14 22:28:04 +0530

    roboshop proj set up

--- Pluralsight/variables.sh
+++ Pluralsight/variables.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 directory=reports1 #creating the variable called directory and giving the value as "reports1"
 mkdir $directory #creating the directory "reports1"
-grep B5 /root/shipments.csv > $directory
+grep B5 /root/shipments.csv > $directory/B5
