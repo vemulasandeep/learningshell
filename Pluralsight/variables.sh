f8e747580d5f92d907a214fb3246cf69e2a2189b
Sandeep <sandeep.vemula88@gmail.com>
2023-05-14 22:39:17 +0530

    testing variables

--- Pluralsight/variables.sh
+++ Pluralsight/variables.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 directory=reports1 #creating the variable called directory and giving the value as "reports1"
 mkdir $directory #creating the directory "reports1"
-grep $1 /root/shipments.csv > $directory/$1.csv
+grep $1 /root/shipments.csv > $directory/$1.csv #here $1 replace with container, while running the code we need to give the container
 echo wrote report $directory/$1.csv
