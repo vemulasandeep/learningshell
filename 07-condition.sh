b8155066b651c6e48627db80b2a7bb1d5c10df79
Sandeep <sandeep.vemula88@gmail.com>
2023-05-17 21:32:22 +0530

    if demo

--- 07-condition.sh
+++ 07-condition.sh
@@ -6,9 +6,16 @@ USER_ID=$(id -u)
 #then echo You are not a root user
 #fi
 
-if [ $USER_ID -eq 0 ]
- then
-    echo root user $USER_ID
-else
-    echo not a root user $USER_ID
+#if [ $USER_ID -eq 0 ]
+# then
+#    echo root user $USER_ID
+#else
+#    echo not a root user $USER_ID
+#fi
+
+if [ $USER_ID -ne 0 ]
+then
+  echo you are not a root user
+  echo You can run this script as root user or sudo
+  exit 1
 fi
