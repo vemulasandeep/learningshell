30e89cb1a6f24f2170f72b357035603504b76ece
Sandeep <sandeep.vemula88@gmail.com>
2023-05-17 22:18:51 +0530

    functions demo

--- 08-functions.sh
+++ 08-functions.sh
@@ -3,6 +3,8 @@
 #declaring a function
 abc() {
 echo ABC Function
+echo a= $a
+b=20
 }
 
 #declaring a function in another way
@@ -11,5 +13,8 @@ function xyz() {
 echo XYZ Function
 }
 #calling a function
+#Main Programme
+a=10
 abc
 xyz
+echo b = $b
