66b92a1be3a4dc4061704dac9ee2486a8ac5e8ef
Sandeep <sandeep.vemula88@gmail.com>
2023-05-17 22:12:50 +0530

    functions demo

---
+++ 08-functions.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+#declaring a function
+abc() {
+echo ABC Function
+}
+
+#declaring a function in another way
+
+function xyz() {
+echo XYZ Function
+}
+#calling a function
+abc
