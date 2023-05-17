1a6f74d502261982e614e06151cef640b484ce8f
Sandeep <sandeep.vemula88@gmail.com>
2023-05-17 20:12:20 +0530

    conditions demo

--- 07-condition.sh
+++ 07-condition.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 USER_ID=$(id -u)
 
-if [ $USER_ID -ne 0 ]
+#if [ $USER_ID -ne 0 ]
+#
+#then echo You are not a root user
+#fi
 
-then echo You are not a root user
+if [ $USER_ID -eq 0 ]
+ then
+    echo root user $USER_ID
+else
+    echo not a root user $USER_ID
 fi
