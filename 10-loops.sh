2646cb6b16f69613d4198fc9b9bb67da6f8b7938
Sandeep <sandeep.vemula88@gmail.com>
2023-05-24 19:43:12 +0530

    Shell case demo

---
+++ 10-loops.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+#Fundamental Loops : while & for (we have until & select loops as well)
+
+#Synyax
+  #loop -- command expression or inputs ; do
+     #commands
+     #done
+
+#While loop
+
+i=0
+
+while [ $1 -lt 2 ]; do
+   echo Hello
+   i=$(($i+1))
+   done
+
+
